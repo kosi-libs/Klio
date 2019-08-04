cd8b9f95dd6354bf520cc65409247517ea4caa22
Salomon BRYS <salomon@kodein.net>
2019-08-04 13:21:03 -0400

    Removed ObjectCache, which was too close to DB to be generic.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,15 @@ plugins {
     id("org.kodein.root")
 }
 
+buildscript {
+    repositories {
+        maven(url = "https://kotlin.bintray.com/kotlinx")
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlinx:atomicfu-gradle-plugin:0.12.9")
+    }
+}
+
 allprojects {
     group = "org.kodein.memory"
     version = "0.1.0"
--- kodein-memory/src/jvmMain/kotlin/org/kodein/memory/concurent/atomicJVM.kt
+++
@@ -1,3 +0,0 @@
-package org.kodein.memory.concurent
-
-actual typealias AtomicInteger = java.util.concurrent.atomic.AtomicInteger
