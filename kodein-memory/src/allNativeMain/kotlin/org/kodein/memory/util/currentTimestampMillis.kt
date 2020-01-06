94de90e4d348802ec5457c6420e97f965f4fe92e
Salomon BRYS <salomon.brys@gmail.com>
2020-01-06 13:35:27 +0100

    Native timestamp uses posix instead of Linux.

--- kodein-memory/src/allNativeMain/kotlin/org/kodein/memory/util/currentTimestampMillis.kt
+++ kodein-memory/src/allNativeMain/kotlin/org/kodein/memory/util/currentTimestampMillis.kt
@@ -3,15 +3,14 @@ package org.kodein.memory.util
 import kotlinx.cinterop.alloc
 import kotlinx.cinterop.memScoped
 import kotlinx.cinterop.ptr
-import platform.linux.ftime
-import platform.linux.timeb
+import platform.posix.gettimeofday
+import platform.posix.timeval
 
 @UseExperimental(ExperimentalUnsignedTypes::class)
 actual fun currentTimestampMillis(): Long {
     memScoped {
-        val time = alloc<timeb>()
-        ftime(time.ptr)
-
-        return time.time * 1000L + time.millitm.toLong()
+        val tv = alloc<timeval>()
+        gettimeofday(tv.ptr, null)
+        return (tv.tv_sec * 1000L) + (tv.tv_usec / 1000L)
     }
 }
