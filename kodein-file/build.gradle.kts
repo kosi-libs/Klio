af909b5d902ee8b92c763f585cf30009bb18a00c
Romain Boisselle <romain@boisselle.dev>
2020-07-26 15:29:19 +0200

    upgrade gradle (+conf)

--- kodein-file/build.gradle.kts
+++ kodein-file/build.gradle.kts
@@ -38,7 +38,7 @@ kodein {
             dependsOn(otherPosix)
         }
 
-        add(kodeinTargets.native.allApple)
+        add(kodeinTargets.native.allDarwin)
 
         // Remove mingwX64 from allPosix
         add(kodeinTargets.native.mingwX64.copy(dependencies = listOf(kodeinSourceSets.allNative)))
